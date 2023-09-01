1fe3e75640b9dcfa7e7c487ea87e77bfadbca973
Francis Abekah <francispkabeka@gmail.com>
2023-09-01 21:39:49 +0000

    Edited App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Pages from "./components/pages/Pages"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Hello World!!</h2>
-        <p>
-          Edit <code>src/App.js</code> and save your project.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Pages />
 }
 
-export default App;
+export default App
