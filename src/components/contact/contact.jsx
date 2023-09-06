335c64aa869818169f14b149a7f0a31b3620cb98
Francis Abekah <francispkabeka@gmail.com>
2023-09-06 20:52:17 +0000

    Input code into header conponent and route

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const contact = () => {
+const Contact = () => {
   return (
     <div>
-      
+      <h1>This is Contact Page</h1>
     </div>
   )
 }
 
-export default contact
+export default Contact;
