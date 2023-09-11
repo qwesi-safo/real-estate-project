99b9199d23089c286888b6558da9a5e814a1883f
osafo <bensakora@gmail.com>
2023-09-11 22:01:47 +0000

    made changes to css and jsx files in the components folder

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react"
+import Back from "../common/Back"
+import RecentCard from "../home/recent/RecentCard"
+import "../home/recent/recent.css"
+import img from "../images/about.jpg"
 
 const Blog = () => {
   return (
-    <div>
-      <h1>This is the Blog Page</h1>
-    </div>
+    <>
+      <section className='blog-out mb'>
+        <Back name='Blog' title='Blog Grid - Our Blogs' cover={img} />
+        <div className='container recent'>
+          <RecentCard />
+        </div>
+      </section>
+    </>
   )
 }
 
-export default Blog;
+export default Blog
