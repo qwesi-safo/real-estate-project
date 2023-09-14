404bd0f90e248d089302f900faaa393eb247ff8e
osafo <bensakora@gmail.com>
2023-09-14 07:32:10 +0000

    made changes to in the pages file

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Routes, Route } from "react-router-dom";
-import home from "../home/home";
+import Home from "../home/Home";
 import Footer from "../common/footer/Footer";
 import Header from "../common/header/Header";
-import about from "../about/about";
-import blog from "../blog/blog";
+import About from "../about/About";
+import Blog from "../blog/Blog";
 import Services from "../services/Services";
-import contact from "../contact/contact";
+import Contact from "../contact/Contact";
 
 
 
@@ -17,11 +17,11 @@ const Pages = () => {
       
         <Header />
         <Routes>
-          <Route exact path='/' Component={home} />
-          <Route exact path='/about' Component={about} />
+          <Route exact path='/' Component={Home} />
+          <Route exact path='/About' Component={About} />
           <Route exact path='/services' Component={Services} />
-          <Route exact path='/blog' Component={blog} />
-          <Route exact path='/contact' Component={contact} />
+          <Route exact path='/Blog' Component={Blog} />
+          <Route exact path='/Contact' Component={Contact} />
         </Routes>
         <Footer />
     </>
